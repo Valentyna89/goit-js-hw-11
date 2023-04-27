68c673a4e01301815a08945630b05b1785d9ec03
Valentyna Kirbaba <lushkirbaba@gmail.com>
2023-04-27 15:23:00 +0200

    almost finished

--- src/index.js
+++ src/index.js
@@ -16,23 +16,99 @@ refs.form.addEventListener('submit', handleSearchForm);
 refs.loadBtn.addEventListener('click', handleLoadMoreBtn);
 refs.loadBtn.classList.add('hidden');
 
-function handleSearchForm(evt) {
-  evt.preventDefault();
+function handleSearchForm(e) {
+  e.preventDefault();
   clearContainer();
-  pixabayApi.searchQuery = evt.currentTarget.elements.searchQuery.value;
+  refs.loadBtn.classList.add('hidden');
+  pixabayApi.searchQuery = e.currentTarget.elements.searchQuery.value;
 
   if (pixabayApi.searchQuery === '') {
     refs.loadBtn.classList.add('hidden');
     Notify.failure(`Please, enter your request`);
     return;
   }
+
   pixabayApi.resetPage();
 
-  refs.loadBtn.classList.remove('hidden');
+  pixabayApi.axiosArticales().then(data => {
+    renderGallery(data);
+    refs.loadBtn.classList.remove('hidden');
+  });
+  //   refs.loadBtn.classList.remove('hidden');
 }
 
 function handleLoadMoreBtn() {
-  pixabayApi.axiosArticales();
+  pixabayApi.axiosArticales().then(renderGallery);
+}
+
+function renderGallery(data) {
+  try {
+    const allPages = Math.ceil(data.totalHits / pixabayApi.per_page);
+    const markupGallery = createGalleryCard(data.hits);
+    pixabayApi.incrementPage();
+    if (data.totalHits === 0) {
+      refs.loadBtn.classList.add('hidden');
+      Notify.failure(
+        `Sorry, there are no images matching your search query. Please try again.`
+      );
+      refs.loadBtn.classList.add('hidden');
+    } else if (pixabayApi.page > allPages && data.hits.length < 40) {
+      refs.loadBtn.classList.add('hidden');
+      Notify.info(`Hooray! We found ${data.totalHits} images.`);
+      Notify.info(`We're sorry, but you've reached the end of search results.`);
+
+      clearContainer();
+    } else if (pixabayApi.page - 1 === 1 && data.hits.length > 1) {
+      Notify.info(`Hooray! We found ${data.totalHits} images.`);
+    } else {
+      refs.loadBtn.classList.remove('hidden');
+    }
+
+    refs.gallery.insertAdjacentHTML('beforeend', markupGallery);
+
+    const lightbox = new SimpleLightbox('.gallery a', {
+      captionDelay: 250,
+    });
+
+    lightbox.refresh();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function createGalleryCard(hits) {
+  console.log(hits);
+  return hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card">
+                    <a class=photo-card__link href="${largeImageURL}"> <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>${likes}
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>${views}
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>${comments}
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>${downloads}
+                         </p>
+                    </div>
+                </div>`;
+      }
+    )
+    .join('');
 }
 
 function clearContainer() {
